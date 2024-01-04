9ba692f0ca86ca4497cc2c2e338280dfee212282
DongHoon Lee <dhl1924@naver.com>
2024-01-05 00:43:20 +0900

    refactor: 방법 리펙토링

--- src/problem/section1/_02/Main.java
+++ src/problem/section1/_02/Main.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 class Main {
     public String solution(String str) {
         StringBuilder convertString = new StringBuilder(str);
-        String upperCaseString = str.toUpperCase();
-        String lowerCaseString = str.toLowerCase();
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == upperCaseString.charAt(i)) {
-                convertString.setCharAt(i, str.toLowerCase().charAt(i));
-            } else if (str.charAt(i) == lowerCaseString.charAt(i)) {
-                convertString.setCharAt(i, str.toUpperCase().charAt(i));
+        for (int i = 0; i < convertString.length(); i++) {
+            char ch = convertString.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                convertString.setCharAt(i, Character.toUpperCase(ch));
+            } else if (Character.isUpperCase(ch)) {
+                convertString.setCharAt(i, Character.toLowerCase(ch));
             }
         }
+
         return convertString.toString();
     }
     public static void main(String[] args) {
