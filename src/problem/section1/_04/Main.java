c95075b87d2c9d458afc9623607863e2da10370c
DongHoon Lee <dhl1924@naver.com>
2024-02-12 23:54:39 +0900

    refactor: 방법 리펙토링

--- src/problem/section1/_04/Main.java
+++ src/problem/section1/_04/Main.java
@@ -1,33 +1,27 @@
 package problem.section1._04;
 
-import java.util.Scanner;
+import java.util.*;
+
 class Main {
-    public String solution(String str) {
-        String answer = "";
-        char[] c = new char[str.length()];
-
-        for (int i = 0; i < str.length(); i++) {
-            c[i] = str.charAt(i);
-        }
-
-        for (int i = str.length() - 1; i >= 0 ; i--) {
-            answer += String.valueOf(c[i]);
+    public ArrayList<String> solution(String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
         }
         return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int count = kb.nextInt();
-        String[] arr = new String[count];
-        for (int i = 0; i < count; i++) {
-            arr[i] = kb.next();
+        String[] str = new String[count];
+        for(int i = 0; i < count; i++) {
+            str[i] = kb.next();
         }
-        for (int i = 0; i < count; i++) {
-            System.out.println(T.solution(arr[i]));
+        for(String x : T.solution(str)) {
+            System.out.println(x);
         }
     }
 }
-
-
