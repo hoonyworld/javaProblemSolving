04f0836e817157355cdc1b076d23d84b6996edae
DongHoon Lee <dhl1924@naver.com>
2024-03-05 21:06:29 +0900

    feat: 중복 문자 제거 기능 구현

--- src/problem/section1/_06/Main.java
+++ src/problem/section1/_06/Main.java
@@ -1,2 +1,27 @@
-package problem.section1._06;public class Main {
+package problem.section1._06;
+
+import java.util.*;
+
+class Main {
+    public String solution(String str) {
+        String answer = "";
+        Set<Character> hash = new LinkedHashSet<>();
+
+        for (Character c : str.toCharArray()) {
+            hash.add(c);
+        }
+
+        for (Character c : hash) {
+            answer += String.valueOf(c);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String duplicateString = kb.next();
+        System.out.println(T.solution(duplicateString));
+    }
 }
