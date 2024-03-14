481b62f0c106ff5c4e71cccd29d9df6031c5dca6
DongHoon Lee <dhl1924@naver.com>
2024-03-14 14:30:08 +0900

    refactor: 방법 리펙토링

--- src/problem/section1/_08/Main.java
+++ src/problem/section1/_08/Main.java
@@ -1,33 +1,18 @@
 package problem.section1._08;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.StringJoiner;
 
 class Main {
     public String solution(String str) {
-//        char[] c = new char[str.length()];
-        List<Character> onlyAlphabet = new ArrayList<>();
-        for (char x : str.toLowerCase().toCharArray()) {
-            if (Character.isAlphabetic(x)) {
-                onlyAlphabet.add(x);
-            }
-        }
+        String answer = "NO";
+        str = str.toLowerCase().replaceAll("[^a-z]", "");
+        String reverseSentence = new StringBuilder(str).reverse().toString();
 
-//        String s = String.valueOf(onlyAlphabet);
-        StringJoiner sj = new StringJoiner("");
-        for (Character c : onlyAlphabet) {
-            sj.add(String.valueOf(c));
-        }
-
-        String s = sj.toString();
-        StringBuilder sb = new StringBuilder(s);
-
-        if (s.equals(sb.reverse().toString())) {
+        if (str.equals(reverseSentence)){
             return "YES";
-        } else
-            return "NO";
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
